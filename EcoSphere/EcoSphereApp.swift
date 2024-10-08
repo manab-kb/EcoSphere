45d6dc44eede81b8b21f2ab830fa93e5ac9ba8e7
Manab Biswas <77844663+manab-kb@users.noreply.github.com>
2024-10-08 14:46:43 +0100

    Initial Commit

---
+++ EcoSphere/EcoSphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoSphereApp.swift
+//  EcoSphere
+//
+//  Created by Manab Kumar Biswas on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct EcoSphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EcoSphereTests/EcoSphereTests.swift
@@ -0,0 +1,17 @@
+//
+//  EcoSphereTests.swift
+//  EcoSphereTests
+//
+//  Created by Manab Kumar Biswas on 08/10/2024.
+//
+
+import Testing
+@testable import EcoSphere
+
+struct EcoSphereTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
